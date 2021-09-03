a6e48d96084c502b6e72552abc60798fa00d092b
Odai Wattad <45322451+odaiwa@users.noreply.github.com>
2021-09-03 14:09:18 +0300

    add product to DB

--- src/app/components/products-area/add-product/add-product.component.ts
+++ src/app/components/products-area/add-product/add-product.component.ts
@@ -1,5 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { ProductModel } from 'src/app/models/product-model';
+import { environment } from 'src/environments/environment';
 
 @Component({
     selector: 'app-add-product',
@@ -10,9 +13,32 @@ export class AddProductComponent {
 
     public product = new ProductModel();
 
+    constructor(private http:HttpClient, private myRouter:Router){}
 
-    public add() {
-        console.log(this.product);
+
+    public setImage(args:Event):void{
+        this.product.image = (args.target as HTMLInputElement).files;
     }
+    
+    public async add() {
+        try {
+            const myFormData = new FormData();
+            myFormData.append("name", this.product.name);
+            myFormData.append("price", this.product.price.toString());
+            myFormData.append("stock", this.product.stock.toString());
+            myFormData.append("image", this.product.image.item(0));
+
+            const addedProduct = await this.http.post<ProductModel>(environment.productsUrl, myFormData).toPromise();
+            alert("Product added. ID: " + addedProduct.id);
+
+            // Redirect:
+            this.myRouter.navigateByUrl("/products");
+        }
+        catch(err) {
+            alert(err.message);
+        }
+    }
+
+
 
 }
